e2b892b6896a7e4c39fafbf9790126bc5c10130c
Andrzej Czajka <andrzej.b.czajka@gmail.com>
2023-08-14 23:08:39 +0200

    first frisco script

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-import time
+from test_steps import Driver
 
 
 def main():
-    driver = webdriver.Chrome(service=Service("chromedriver.exe"))
-    driver.get("https://www.softest.com.pl")
-    time.sleep(5)
-    print(driver.title)
+    driver = Driver()
+    Driver.setup(driver)
 
 
 if __name__ == '__main__':
