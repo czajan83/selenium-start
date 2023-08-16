505042eace60f5d7bdec5d2e420741fa3cd3f25b
Andrzej Czajka <andrzej.b.czajka@gmail.com>
2023-08-16 18:41:05 +0200

    added add_to_basket method

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from test_steps import Driver
 def main():
     driver = Driver()
     Driver.setup(driver)
+    Driver.add_to_basket(cytryny)
 
 
 if __name__ == '__main__':
--- test_steps.py
+++ test_steps.py
@@ -1,12 +1,15 @@
 import time
 
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
+COOKIES_ACC_BTN_XPATH = f"//*[@id=\"wrapper\"]/div[1]/div/div/div[2]/div/button[1]"
+SEARCH_EDITTEXT_XPATH = f"//*[@id=\"header\"]/div[1]/div/div[2]/div/div/form/input"
 
 class Driver:
     def __init__(self, browser="chrome"):
@@ -17,8 +20,13 @@ class Driver:
 
     def setup(self, website="https://www.frisco.pl"):
         self.driver.get(website)
-        self.wait.until(ec.presence_of_element_located((By.XPATH, "//*[@id=\"wrapper\"]/div[1]/div/div/div[2]/div/button[1]")))
-        self.driver.find_element(By.XPATH, "//*[@id=\"wrapper\"]/div[1]/div/div/div[2]/div/button[1]").click()
-        self.driver.find_element(By.XPATH, "//*[@id=\"header\"]/div[1]/div/div[2]/div/div/form/input").\
-            send_keys("cytryny\n")
-        time.sleep(15)
+        self.wait.until(ec.presence_of_element_located((By.XPATH, COOKIES_ACC_BTN_XPATH)))
+        self.driver.find_element(By.XPATH, COOKIES_ACC_BTN_XPATH).click()
+
+    def add_to_basket(self, product, amount):
+        self.driver.find_element(By.XPATH, SEARCH_EDITTEXT_XPATH).send_keys(f"{product}\n")
+        try:
+            print(self.driver.find_element(By.XPATH,
+                                           "//*[@id=\"page-content\"]/div/div[2]/div/div[3]/div/div[4]/div/div/div/div/div[3]/div[3]/a/span[2]/span").text)
+        except NoSuchElementException:
+            print("Unable to find the element on website")
